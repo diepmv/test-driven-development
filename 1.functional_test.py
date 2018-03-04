fae1423357bd7b6a051098de374cc67acc80a182
diepmv <maidiep.ptit@gmail.com>
2018-03-04 10:09:46 +0700

    delete runk file

--- 1.functional_test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-browser = webdriver.Firefox()
-try:
-  browser.get("http://localhost:8000")
-except:
-  pass
-print(browser.title, 7777)
-
-assert "Django" in browser.title
